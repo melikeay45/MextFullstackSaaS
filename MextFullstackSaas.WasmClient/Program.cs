4bb4c35cb862fd4537b749c258d2fa8e52f532db
Melike Aydın <melike.ay45@gmail.com>
2024-06-12 13:33:49 +0300

    [Fix]: Fixed loading page. [Feat]: Added LocalStorage.

--- MextFullstackSaas.WasmClient/Program.cs
+++ MextFullstackSaas.WasmClient/Program.cs
@@ -1,8 +1,11 @@
+using Blazored.LocalStorage;
 using Blazored.Toast;
 using MextFullstackSaas.WasmClient;
 using MextFullstackSaas.WasmClient.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -15,5 +18,16 @@ builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(apiUrl) 
 builder.Services.AddBlazoredToast();
 
 builder.Services.AddScoped<IToasterService,BlazoredToasterManager>();
+
+builder.Services.AddBlazoredLocalStorage(config =>
+{
+    config.JsonSerializerOptions.DictionaryKeyPolicy = JsonNamingPolicy.CamelCase;
+    config.JsonSerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull;
+    config.JsonSerializerOptions.IgnoreReadOnlyProperties = true;
+    config.JsonSerializerOptions.PropertyNameCaseInsensitive = true;
+    config.JsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
+    config.JsonSerializerOptions.ReadCommentHandling = JsonCommentHandling.Skip;
+    config.JsonSerializerOptions.WriteIndented = false;
+});
 
 await builder.Build().RunAsync();
